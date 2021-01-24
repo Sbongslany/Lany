67f416a9dd481a9553ce04ce4e169d965e12c596
unknown <you@example.com>
2021-01-24 21:51:09 +0200

    'update'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import { React } from "react";
+import  React  from "react";
 import './App.css';
+import  Home  from "./components";
 
 function App() {
   return (
     <>
-    <h2>Welcome to the component</h2>
+    <Home/>
     </>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { 
+    AppBar,
+    Toolbar,
+    ListItem,
+    IconButton,
+    ListItemText,
+    Avatar,
+    Divider,
+    List,
+    Typography,
+    Box
+
+ } from "@material-ui/core";
+import {
+    ArrowBack,
+    AssignmentInd,
+    Home,
+    App
+} from '@material-ui/icons'
+
+const Navbar = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Navbar
